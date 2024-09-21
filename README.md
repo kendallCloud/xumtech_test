d07548211f9b1c4749741a419897e06d11a817f8
Kendall Granados Barrantes <76669386+kendallCloud@users.noreply.github.com>
2024-09-21 08:25:33 -0600

    better scss functionality

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## 🌟 Introduction
-I'm Kendall Granados B. , i'm a software engineer and this my proposed solution for the chatbot challenge for Xumtech 
+Hi, I'm Kendall Granados B. , i'm a software engineer and this my proposed solution for the chatbot challenge for Xumtech 
 
 ## Project description
+
+
+
--- frontend/chatbot-xumtech/src/App.jsx
+++ frontend/chatbot-xumtech/src/App.jsx
@@ -1,12 +1,41 @@
+import React, { useState } from 'react';
 import '../src/styles.scss';
-import Chatbot from './components/chatbot'
+import Chatbot from './components/Chatbot';
 import UnsolvedManager from './components/UnsolvedManager';
 
 export default function App() {
+  const [showChatbot, setShowChatbot] = useState(false);
+  const [showUnsolvedManager, setShowUnsolvedManager] = useState(false);
+
   return (
     <>
-      <img style={{ maxWidth: '50%', paddingLeft:'25%'}} src="" alt="SVG LOGO" />
-            <UnsolvedManager/>
+      <img style={{ maxWidth: '25%', paddingLeft: '25%' }} src="/banking.svg" alt="SVG LOGO" />
+      <div className="button-container">
+        <button className="right-button" onClick={() => {
+          setShowChatbot(true)
+          setShowUnsolvedManager(false)
+        }
+          }>Chatbot</button>
+        <button className="right-button" onClick={() => {
+          setShowChatbot(true)
+          setShowUnsolvedManager(false)
+        }}>Unsolved Manager</button>
+      </div>
+
+      {/* Conditionally rendering components */}
+      {showChatbot && (
+        <div className="modal">
+          <Chatbot />
+          <button className="close-button" onClick={() => setShowChatbot(false)}>Cerrar</button>
+        </div>
+      )}
+      
+      {showUnsolvedManager && (
+        <div className="modal">
+          <UnsolvedManager />
+          <button className="close-button" onClick={() => setShowUnsolvedManager(false)}>Cerrar</button>
+        </div>
+      )}
     </>
   );
 }
